52faaaa9035da815439a4c60101a847061b8f29a
Luka Sotra <lukasotra@gmail.com>
2024-11-13 19:14:10 +0100

    feat: implement `GetSecretValueRequest` for aws secrets

--- aws/AwsSecretsManager.cs
+++ aws/AwsSecretsManager.cs
@@ -5,5 +5,29 @@ namespace common_dotnet.aws;
 
 public class AwsSecretsManager
 {
+    private readonly IAmazonSecretsManager _secretsManager;
+
+    public AwsSecretsManager(IAmazonSecretsManager secretsManager)
+    {
+        _secretsManager = secretsManager;
+    }
     
+    public async Task<string> GetSecretValueAsync(string secretName)
+    {
+        try
+        {
+            var request = new GetSecretValueRequest
+            {
+                SecretId = secretName
+            };
+
+            GetSecretValueResponse? response = await _secretsManager.GetSecretValueAsync(request);
+            return response.SecretString;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error retrieving secret: {ex.Message}");
+            throw;
+        }
+    }
 }
