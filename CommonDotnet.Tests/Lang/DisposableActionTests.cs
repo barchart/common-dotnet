48226f520edffd2d61c212cccc65232d74e8225c
Luka Sotra <lukasotra@gmail.com>
2024-11-13 22:22:24 +0100

    test: add unit tests for `Disposable` and `DisposableAction`

---
+++ CommonDotnet.Tests/Lang/DisposableActionTests.cs
@@ -0,0 +1,51 @@
+#region Using Statements
+
+using CommonDotnet.Lang;
+
+#endregion
+
+namespace CommonDotnet.Tests.Lang;
+
+public class DisposableActionTests
+{
+    #region Fields
+    
+    private readonly ITestOutputHelper _testOutputHelper;
+    
+    #endregion
+
+    #region Constructor(s)
+    
+    public DisposableActionTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    #endregion
+
+    #region Test Methods (Constructor)
+
+    [Fact]
+    public void Constructor_NullAction_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new DisposableAction(null!));
+    }
+    
+    #endregion
+    
+    #region Test Methods (Dispose)
+    
+    [Fact]
+    public void Dispose_ExecutesAction()
+    {
+        bool actionExecuted = false;
+        Action action = () => actionExecuted = true;
+        DisposableAction disposableAction = new(action);
+
+        disposableAction.Dispose();
+
+        Assert.True(actionExecuted);
+    }
+    
+    #endregion
+}
--- CommonDotnet.Tests/Lang/DisposableTests.cs
+++ CommonDotnet.Tests/Lang/DisposableTests.cs
@@ -8,28 +8,92 @@ namespace CommonDotnet.Tests.Lang;
 
 public class DisposableTests
 {
+    #region Fields
+    
     private readonly ITestOutputHelper _testOutputHelper;
+    
+    #endregion
 
+    #region Constructor(s)
+    
     public DisposableTests(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
     }
     
+    #endregion
+    
+    #region Test Methods (Dispose)
+    
     [Fact]
-    public void Dispose_ExecutesAction()
+    public void Dispose_SetsIsDisposed()
+    {
+        Disposable disposable = new();
+
+        disposable.Dispose();
+
+        Assert.True(disposable.IsDisposed);
+    }
+
+    [Fact]
+    public void Dispose_CallsOnDispose()
+    {
+        bool onDisposeCalled = false;
+        var disposable = new TestDisposable(() => onDisposeCalled = true);
+
+        disposable.Dispose();
+
+        Assert.True(onDisposeCalled);
+    }
+
+    #endregion
+
+    #region Test Methods (FromAction)
+
+    [Fact]
+    public void FromAction_CreatesDisposableAction()
     {
         bool actionExecuted = false;
         Action action = () => actionExecuted = true;
-        Disposable disposable = new(action);
 
+        var disposable = Disposable.FromAction(action);
         disposable.Dispose();
 
         Assert.True(actionExecuted);
     }
 
+    #endregion
+
+    #region Test Methods (GetEmpty)
+    
     [Fact]
-    public void Constructor_NullAction_ThrowsArgumentNullException()
+    public void GetEmpty_CreatesDisposableThatDoesNothing()
     {
-        Assert.Throws<ArgumentNullException>(() => new Disposable(null!));
+        var disposable = Disposable.GetEmpty();
+
+        disposable.Dispose();
+
+        Assert.True(disposable.IsDisposed);
     }
+    
+    #endregion
+
+    #region Nested Types
+
+    private class TestDisposable : Disposable
+    {
+        private readonly Action _onDispose;
+
+        public TestDisposable(Action onDispose)
+        {
+            _onDispose = onDispose;
+        }
+
+        protected override void OnDispose()
+        {
+            _onDispose();
+        }
+    }
+    
+    #endregion
 }
