75f058613d850f74c6b1e02a4693d77628c3f747
Luka Sotra <lukasotra@gmail.com>
2024-11-13 22:00:18 +0100

    feat: add `DisposableAction`

---
+++ CommonDotnet/Lang/DisposableAction.cs
@@ -0,0 +1,45 @@
+namespace CommonDotnet.Lang;
+
+/// <summary>
+///     Represents an action that will be executed when the object is disposed.
+/// </summary>
+public class DisposableAction : IDisposable
+{
+    #region Fields
+
+    private readonly Action _action;
+
+    #endregion
+
+    #region Constructor(s)
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="DisposableAction"/> class with the specified action.
+    /// </summary>
+    /// <param name="disposableAction">
+    ///     The action to execute when the object is disposed.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///     Thrown when the <paramref name="disposableAction"/> is null.
+    /// </exception>
+    public DisposableAction(Action disposableAction)
+    {
+        _action = disposableAction ?? throw new ArgumentNullException(nameof(disposableAction));
+    }
+
+    #endregion
+
+    #region Methods
+    
+    /// <summary>
+    ///     Executes the action specified during the initialization of the object.
+    /// </summary>
+    public void Dispose()
+    {
+        _action.Invoke();
+        
+        GC.SuppressFinalize(this);
+    }
+    
+    #endregion
+}
