28d93ac04a5e6388469fdb7a9f5be930fe65f0cf
Luka Sotra <lukasotra@gmail.com>
2024-11-19 13:34:05 +0100

    refactor: reorganize code

--- Barchart.Common.Tests/Aws/AwsSecretsManagerTests.cs
+++ Barchart.Common.Tests/Aws/AwsSecretsManagerTests.cs
@@ -12,8 +12,49 @@ using Barchart.Common.Aws;
 
 namespace Barchart.Common.Tests.Aws;
 
-public class AwsSecretsManagerTests(ITestOutputHelper testOutputHelper)
+public class AwsSecretsManagerTests
 {
+    #region Fields
+    
+    private readonly ITestOutputHelper _testOutputHelper;
+    
+    #endregion
+
+    #region Constructor(s)
+
+    public AwsSecretsManagerTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    #endregion
+    
+    #region Test Methods (GetSecret)
+    
+    [Fact]
+    public async Task GetSecret_SecretValue_ReturnsString()
+    {
+        Mock<IAmazonSecretsManager> mockSecretsManager = new Mock<IAmazonSecretsManager>();
+        string secretJson = "{\"username\":\"luka\",\"password\":\"mock\"}";
+        GetSecretValueResponse response = new()
+        {
+            SecretString = secretJson
+        };
+
+        mockSecretsManager
+            .Setup(x => x.GetSecretValueAsync(It.IsAny<GetSecretValueRequest>(), default))
+            .ReturnsAsync(response);
+
+        AwsSecretsManager awsSecretsManager = new(mockSecretsManager.Object);
+
+        string secretString = await awsSecretsManager.GetSecret("TEST_SECRET");
+
+        Assert.NotNull(secretString);
+        Assert.Equal(secretJson, secretString);
+    }
+
+    #endregion
+    
     #region Test Methods (GetSecret<T>)
     
     [Fact]
@@ -55,8 +96,7 @@ public class AwsSecretsManagerTests(ITestOutputHelper testOutputHelper)
 
         AwsSecretsManager awsSecretsManager = new(mockSecretsManager.Object);
 
-        await Assert.ThrowsAsync<JsonException>(async () =>
-            await awsSecretsManager.GetSecret<Secret>("TEST_SECRET"));
+        await Assert.ThrowsAsync<JsonException>(async () => await awsSecretsManager.GetSecret<Secret>("TEST_SECRET"));
     }
     
     [Fact]
@@ -74,47 +114,34 @@ public class AwsSecretsManagerTests(ITestOutputHelper testOutputHelper)
 
         AwsSecretsManager awsSecretsManager = new(mockSecretsManager.Object);
 
-        await Assert.ThrowsAsync<JsonException>(async () =>
-            await awsSecretsManager.GetSecret<Secret>("TEST_SECRET"));
+        await Assert.ThrowsAsync<JsonException>(async () => await awsSecretsManager.GetSecret<Secret>("TEST_SECRET"));
     }
     
-    #endregion
-
-    #region Test Methods (GetSecret)
-    
-    [Fact]
-    public async Task GetSecret_SecretValue_ReturnsString()
-    {
-        Mock<IAmazonSecretsManager> mockSecretsManager = new Mock<IAmazonSecretsManager>();
-        string secretJson = "{\"username\":\"luka\",\"password\":\"mock\"}";
-        GetSecretValueResponse response = new()
-        {
-            SecretString = secretJson
-        };
-
-        mockSecretsManager
-            .Setup(x => x.GetSecretValueAsync(It.IsAny<GetSecretValueRequest>(), default))
-            .ReturnsAsync(response);
-
-        AwsSecretsManager awsSecretsManager = new(mockSecretsManager.Object);
-
-        string secretString = await awsSecretsManager.GetSecret("TEST_SECRET");
-
-        Assert.NotNull(secretString);
-        Assert.Equal(secretJson, secretString);
-    }
-
     #endregion
     
     #region Nested Types
 
-    private class Secret(string username, string password)
+    private class Secret
     {
+        #region Properties
+
         [JsonPropertyName("username")]
-        public string Username { get; } = username;
+        public string Username { get; }
 
         [JsonPropertyName("password")]
-        public string Password { get; } = password;
+        public string Password { get; }
+        
+        #endregion
+
+        #region Constructor(s)
+
+        public Secret(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        #endregion
     }
 
     #endregion
