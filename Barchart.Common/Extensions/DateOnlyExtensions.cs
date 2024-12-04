3456841b2549fb20461f6e568bbc621070c7a0b5
Luka Sotra <lukasotra@gmail.com>
2024-12-04 15:49:07 +0100

    feat: add `CountDaysBetween` method

--- Barchart.Common/Extensions/DateOnlyExtensions.cs
+++ Barchart.Common/Extensions/DateOnlyExtensions.cs
@@ -18,7 +18,24 @@ public static class DateOnlyExtensions
     /// </returns>
     public static int GetDaysSinceEpoch(this DateOnly date)
     {
-        return date.DayNumber - DateOnly.MinValue.DayNumber;
+        return CountDaysBetween(DateOnly.MinValue, date);
+    }
+    
+    /// <summary>
+    ///     Gets the number of days between two dates.
+    /// </summary>
+    /// <param name="from">
+    ///     The date to start counting from.
+    /// </param>
+    /// <param name="to">
+    ///     The date to count to.
+    /// </param>
+    /// <returns>
+    ///     The number of days between the two dates.
+    /// </returns>
+    public static int CountDaysBetween(this DateOnly from, DateOnly to)
+    {
+        return to.DayNumber - from.DayNumber;
     }
     
     #endregion
