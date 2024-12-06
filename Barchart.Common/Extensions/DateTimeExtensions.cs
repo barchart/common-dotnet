8f9c39427471fcc3d9e184581d3a435f111f6d6e
Luka Sotra <lukasotra@gmail.com>
2024-12-06 19:08:53 +0100

    feat: add `DateTimeExtensions`

---
+++ Barchart.Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+namespace Barchart.Common.Extensions;
+
+/// <summary>
+///     Provides extension methods for the <see cref="DateTime"/> type.
+/// </summary>
+public static class DateTimeExtensions
+{
+    /// <summary>
+    ///     Gets the number of milliseconds since the epoch (1970-01-01).
+    /// </summary>
+    /// <param name="date">
+    ///     The date to get the number of milliseconds since the epoch for.
+    /// </param>
+    /// <returns>
+    ///    The number of milliseconds since the epoch.
+    /// </returns>
+    public static long GetMillisecondsSinceUnixEpoch(this DateTime date)
+    {
+        TimeSpan timeSpan = date - DateTime.UnixEpoch;
+
+        return Convert.ToInt64(timeSpan.TotalMilliseconds);
+    }
+}
